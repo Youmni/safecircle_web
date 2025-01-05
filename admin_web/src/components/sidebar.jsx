6c1477cd05c1edb201f26065e29e44a235723aa4
Youmni <youmni.malha@gmail.com>
2025-01-05 14:53:05 +0100

    Updated home page and added a map for the alerts

--- admin_web/src/components/sidebar.jsx
+++ admin_web/src/components/sidebar.jsx
@@ -16,7 +16,7 @@ const SideBar = () => {
 
   return (
     <div
-      className={`flex flex-col h-screen bg-navy-800 text-white ${
+      className={`flex flex-col min-h-screen bg-navy-800 text-white ${
         isOpen ? "w-64" : "w-16"
       } transition-all duration-300`}
     >
@@ -25,7 +25,7 @@ const SideBar = () => {
         onClick={() => setIsOpen(!isOpen)}
       >
         <button className="text-2xl focus:outline-none">
-          {isOpen ? <FiChevronLeft/> : <FiChevronRight/>}
+          {isOpen ? <FiChevronLeft /> : <FiChevronRight />}
         </button>
       </div>
 
@@ -35,40 +35,50 @@ const SideBar = () => {
             <Link
               to="/home"
               className="flex items-center gap-4 p-2 rounded-md hover:bg-navy-600 cursor-pointer"
-            ><FiHome size={24} />
-              {isOpen && <span>Home</span>}</Link>
+            >
+              <FiHome size={24} />
+              {isOpen && <span>Home</span>}
+            </Link>
           </li>
 
           <li>
             <Link
               to="/users"
               className="flex items-center gap-4 p-2 rounded-md hover:bg-navy-600 cursor-pointer"
-            ><FiUsers size={24} />
-              {isOpen && <span>Users</span>}</Link>
+            >
+              <FiUsers size={24} />
+              {isOpen && <span>Users</span>}
+            </Link>
           </li>
 
           <li>
             <Link
               to="/circles"
               className="flex items-center gap-4 p-2 rounded-md hover:bg-navy-600 cursor-pointer"
-            ><FiCircle size={24} />
-              {isOpen && <span>Circles</span>}</Link>
+            >
+              <FiCircle size={24} />
+              {isOpen && <span>Circles</span>}
+            </Link>
           </li>
 
           {/* <li>
             <Link
               to="/reports"
               className="flex items-center gap-4 p-2 rounded-md hover:bg-navy-600 cursor-pointer"
-            ><FiFileText size={24} />
-              {isOpen && <span>Reports</span>}</Link>
+            >
+              <FiFileText size={24} />
+              {isOpen && <span>Reports</span>}
+            </Link>
           </li> */}
 
           <li>
             <Link
               to="/events"
               className="flex items-center gap-4 p-2 rounded-md hover:bg-navy-600 cursor-pointer"
-            ><FiCalendar size={24} />
-              {isOpen && <span>Events</span>}</Link>
+            >
+              <FiCalendar size={24} />
+              {isOpen && <span>Events</span>}
+            </Link>
           </li>
         </ul>
       </div>
--- admin_web/src/pages/home.jsx
+++ admin_web/src/pages/home.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { FaUser, FaBell, FaFileAlt, FaRegCalendarAlt } from "react-icons/fa";
 import { AuthContext } from "../components/authProvider";
 import { FiLoader } from "react-icons/fi";
+import { Map, Marker, Overlay } from "pigeon-maps";
 
 const Home = () => {
   const { accessToken } = useContext(AuthContext);
@@ -14,6 +15,7 @@ const Home = () => {
     events: "...",
   });
   const [loading, setLoading] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -33,14 +35,21 @@ const Home = () => {
         };
 
         data.forEach((item) => {
-          if (item.className.toLowerCase() === "users") {
-            newStatistics.users = item.amount;
-          } else if (item.className.toLowerCase() === "alerts") {
-            newStatistics.alerts = item.amount;
-          } else if (item.className.toLowerCase() === "reports") {
-            newStatistics.reports = item.amount;
-          } else if (item.className.toLowerCase() === "events") {
-            newStatistics.events = item.amount;
+          switch (item.className.toLowerCase()) {
+            case "users":
+              newStatistics.users = item.amount;
+              break;
+            case "alerts":
+              newStatistics.alerts = item.amount;
+              break;
+            case "reports":
+              newStatistics.reports = item.amount;
+              break;
+            case "events":
+              newStatistics.events = item.amount;
+              break;
+            default:
+              break;
           }
         });
 
@@ -51,7 +60,7 @@ const Home = () => {
         console.error("Error fetching statistics:", error);
         setLoading(false);
       });
-  }, []);
+  }, [accessToken]);
 
   useEffect(() => {
     setLoading(true);
@@ -69,33 +78,44 @@ const Home = () => {
         console.error("Error fetching locations:", error);
         setLoading(false);
       });
-  }, []);
+  }, [accessToken]);
+
+  const handleMarkerClick = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedLocation(null);
+  };
 
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-8">Dashboard</h1>
 
       <div className="flex justify-between mb-8">
-        <div className="flex flex-col items-center bg-white p-6 shadow rounded-lg w-1/5">
-          <FaUser className="text-blue-500 text-4xl mb-2" />
-          <h2 className="text-lg font-semibold text-gray-700">Users</h2>
-          <span className="text-xl font-bold text-black">{statistics.users}</span>
-        </div>
-        <div className="flex flex-col items-center bg-white p-6 shadow rounded-lg w-1/5">
-          <FaBell className="text-green-500 text-4xl mb-2" />
-          <h2 className="text-lg font-semibold text-gray-700">Alerts</h2>
-          <span className="text-xl font-bold text-black">{statistics.alerts}</span>
-        </div>
-        <div className="flex flex-col items-center bg-white p-6 shadow rounded-lg w-1/5">
-          <FaFileAlt className="text-orange-500 text-4xl mb-2" />
-          <h2 className="text-lg font-semibold text-gray-700">Reports</h2>
-          <span className="text-xl font-bold text-black">{statistics.reports}</span>
-        </div>
-        <div className="flex flex-col items-center bg-white p-6 shadow rounded-lg w-1/5">
-          <FaRegCalendarAlt className="text-purple-500 text-4xl mb-2" />
-          <h2 className="text-lg font-semibold text-gray-700">Events</h2>
-          <span className="text-xl font-bold text-black">{statistics.events}</span>
-        </div>
+        {Object.entries(statistics).map(([key, value]) => (
+          <div
+            key={key}
+            className="flex flex-col items-center bg-white p-6 shadow rounded-lg w-1/5"
+          >
+            {key === "users" && (
+              <FaUser className="text-blue-500 text-4xl mb-2" />
+            )}
+            {key === "alerts" && (
+              <FaBell className="text-green-500 text-4xl mb-2" />
+            )}
+            {key === "reports" && (
+              <FaFileAlt className="text-orange-500 text-4xl mb-2" />
+            )}
+            {key === "events" && (
+              <FaRegCalendarAlt className="text-purple-500 text-4xl mb-2" />
+            )}
+            <h2 className="text-lg font-semibold text-gray-700">
+              {key.charAt(0).toUpperCase() + key.slice(1)}
+            </h2>
+            <span className="text-xl font-bold text-black">{value}</span>
+          </div>
+        ))}
       </div>
 
       {loading && (
@@ -104,62 +124,114 @@ const Home = () => {
         </div>
       )}
 
+      <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+        Recent Alerts in the Last 24 Hours
+      </h2>
       <div className="w-full max-h-96 overflow-y-auto">
-      <h2 className="text-2xl font-semibold text-gray-800 mb-4">Last 24 hours</h2>
-      <table className="min-w-full table-auto border-collapse bg-white shadow rounded-lg">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
-                Name
-              </th>
-              <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
-                Status
-              </th>
-              <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
-                Description
-              </th>
-              <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
-                Location
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {locations.length > 0 ? (
-              locations.map((location, index) => (
-                <tr key={index}>
-                  <td className="py-2 px-4 border-b text-sm text-gray-700">
-                    {location.firstName} {location.lastName}
-                  </td>
-                  <td className="py-2 px-4 border-b text-sm text-gray-700">
-                    {location.status}
-                  </td>
-                  <td className="py-2 px-4 border-b text-sm text-gray-700">
-                    {location.description}
-                  </td>
-                  <td className="py-2 px-4 border-b text-sm text-gray-700">
-                    <a
-                      href={`https://www.google.com/maps?q=${location.location.latitude},${location.location.longitude}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 hover:underline"
-                    >
-                      View on Google Maps
-                    </a>
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-auto border-collapse bg-white shadow rounded-lg">
+            <thead className="bg-gray-100 sticky top-0 z-10">
+              <tr>
+                <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
+                  Name
+                </th>
+                <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
+                  Status
+                </th>
+                <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
+                  Description
+                </th>
+                <th className="py-2 px-4 border-b text-left text-sm font-semibold text-gray-700">
+                  Location
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {locations.length > 0 ? (
+                locations.map((location, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b text-sm text-gray-700">
+                      {location.firstName} {location.lastName}
+                    </td>
+                    <td className="py-2 px-4 border-b text-sm text-gray-700">
+                      {location.status}
+                    </td>
+                    <td className="py-2 px-4 border-b text-sm text-gray-700">
+                      {location.description}
+                    </td>
+                    <td className="py-2 px-4 border-b text-sm text-gray-700">
+                      <a
+                        href={`https://www.google.com/maps?q=${location.location.latitude},${location.location.longitude}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline"
+                      >
+                        View on Google Maps
+                      </a>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan="4"
+                    className="py-2 px-4 text-center text-sm text-gray-500"
+                  >
+                    No data available
                   </td>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td
-                  colSpan="4"
-                  className="py-2 px-4 text-center text-sm text-gray-500"
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="w-full max-h-96 overflow-hidden">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          Map Locations in the Last 24 Hours
+        </h2>
+        <Map height={500} defaultCenter={[51.505, -0.09]} defaultZoom={12}>
+          {locations.map((location, index) => (
+            <Marker
+              key={index}
+              width={50}
+              anchor={[location.location.latitude, location.location.longitude]}
+              onClick={() => handleMarkerClick(location)}
+            />
+          ))}
+          {selectedLocation && (
+            <Overlay
+              anchor={[
+                selectedLocation.location.latitude,
+                selectedLocation.location.longitude,
+              ]}
+              offset={[0, 10]}
+            >
+              <div className="bg-white text-black p-2 rounded shadow">
+                <strong>
+                  {selectedLocation.firstName} {selectedLocation.lastName}
+                </strong>
+                <br />
+                {selectedLocation.description}
+                <br />
+                <a
+                  href={`https://www.google.com/maps?q=${selectedLocation.location.latitude},${selectedLocation.location.longitude}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline"
                 >
-                  No data available
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+                  View on Google Maps
+                </a>
+                <button
+                  onClick={handleClosePopup}
+                  className="ml-2 text-red-500 hover:underline"
+                >
+                  Close
+                </button>
+              </div>
+            </Overlay>
+          )}
+        </Map>
       </div>
     </div>
   );
